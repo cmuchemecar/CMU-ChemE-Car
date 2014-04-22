d01de71835aa59835d398493d78f38617ba0775d
Vishnu Razdan <vishnu.razdan@gmail.com>
2014-04-22 16:43:56 -0400

    further style of temperature sensor code and removed a blank file

--- 2014-2015/Miscellaneous/TempCell/TempCell.ino
+++ 2014-2015/Miscellaneous/TempCell/TempCell.ino
@@ -7,6 +7,7 @@ OneWire ds(DS18S20_Pin); // on digital pin 2
 void setup(){
   Serial.begin(9600); //transmission is 9600 bits per second 
 }
+
 String floatPrint(float x){
   int intp = x;
   int fracp = (x - intp) * 1000;
@@ -16,52 +17,39 @@ String floatPrint(float x){
   return (String(intp) + "." + String(fracp));
 }
 
-float getTemp(){
 //returns the temperature from one DS18S20 in DEG Celsius
+float getTemp(){
+  byte data[12];
+  byte addr[8];
+  if ( !ds.search(addr)) {
+    //no more sensors on chain, reset search
+    ds.reset_search();
+    return -1000;
+  }
+  if ( OneWire::crc8( addr, 7) != addr[7]) {
+    Serial.println("CRC is not valid!");
+    return -1000;
+  }
+  if ( addr[0] != 0x10 && addr[0] != 0x28) {
+    Serial.print("Device is not recognized");
+    return -1000;
+  }
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1); // start conversion, with parasite power on at the end
+  byte present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE); // Read Scratchpad
+  for (int i = 0; i < 9; i++) { // we need 9 bytes
+    data[i] = ds.read();
+  }
+  ds.reset_search();
+  byte MSB = data[1];
+  byte LSB = data[0];
+  float tempRead = ((MSB << 8) | LSB); //using two's compliment
+  float TemperatureSum = tempRead / 16;
 
-byte data[12];
-byte addr[8];
-
-if ( !ds.search(addr)) {
-//no more sensors on chain, reset search
-ds.reset_search();
-return -1000;
-}
-
-if ( OneWire::crc8( addr, 7) != addr[7]) {
-Serial.println("CRC is not valid!");
-return -1000;
-}
-
-if ( addr[0] != 0x10 && addr[0] != 0x28) {
-Serial.print("Device is not recognized");
-return -1000;
-}
-
-
-ds.reset();
-ds.select(addr);
-ds.write(0x44,1); // start conversion, with parasite power on at the end
-
-byte present = ds.reset();
-ds.select(addr);
-ds.write(0xBE); // Read Scratchpad
-
-
-for (int i = 0; i < 9; i++) { // we need 9 bytes
-data[i] = ds.read();
-}
-
-ds.reset_search();
-
-byte MSB = data[1];
-byte LSB = data[0];
-
-float tempRead = ((MSB << 8) | LSB); //using two's compliment
-float TemperatureSum = tempRead / 16;
-
-return TemperatureSum;
-
+  return TemperatureSum;
 }
 
 void loop(){
@@ -69,3 +57,4 @@ void loop(){
   Serial.println(floatPrint(temperature));
   delay(100);
 }
+
