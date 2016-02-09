c6a922c3df0fb9e670ce1a63a6bf147517719d80
Xavier Artache <xaxafour@yahoo.com>
2016-02-08 19:09:18 -0500

    bluetoothSerial saves data to file

--- 2015-2016/bluetoothSerial.py
+++ 2015-2016/bluetoothSerial.py
@@ -1,17 +1,20 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/cu.AdafruitEZ-Link4e39-SPP', 9600)
+#ser = serial.Serial('/dev/cu.AdafruitEZ-Link4e39-SPP', 9600)
+ser = serial.Serial('COM27', 9600)
 
 textFile = open("data.txt", 'w')
+start = False
+stop = False
 
 while True:
-    start = False
-    stop = False
     msg = ser.readline()
-    if msg == "Start":
+    print (msg)
+
+    if msg.strip() == "Start":
         start = True
-    if msg == "Stop":
+    if msg.strip() == "Stop":
         stop = True
     if (start and not stop):
         print (msg)
