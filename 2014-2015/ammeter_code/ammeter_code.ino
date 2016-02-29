1de22f56cec33b16acaf9f86235707104d8a6637
Xavier Artache <xaxafour@yahoo.com>
2016-02-29 18:35:35 -0500

    Updated ammeter and battery testing code

--- 2014-2015/ammeter_code/ammeter_code.ino
+++ 2014-2015/ammeter_code/ammeter_code.ino
@@ -10,7 +10,7 @@ double ReadVout(double t, double v)
   {
     double total_delay = t;
     double value_delay = v;
-    double voltage_sum = 0, Vout = 0;
+    double voltage_sum = 0.0, Vout = 0.0;
     int i = 0, count = 0;
     while(i<=total_delay)
     {
@@ -22,15 +22,32 @@ double ReadVout(double t, double v)
     Vout = voltage_sum/count;
     return Vout;
   }
+  
 double ReadCurrent(double t, double v)
   {
     double Vout = ReadVout(t,v);
+    return Vout;
     double Vref = 1.0, Vmax = 4.0;
-    double Cmax = 2;
+    double Cmax = 2.0;
     double ratio = (Vout-Vref)/(Vmax-Vref);
     double current = ratio*Cmax;
     return current;
   }
+  
+void printDouble( double val, unsigned int precision){
+// prints val with number of decimal places determine by precision
+// NOTE: precision is 1 followed by the number of zeros for the desired number of decimial places
+// example: printDouble( 3.1415, 100); // prints 3.14 (two decimal places)
+
+   Serial.print (int(val));  //prints the int part
+   Serial.print("."); // print the decimal point
+   unsigned int frac;
+   if(val >= 0)
+       frac = (val - int(val)) * precision;
+   else
+       frac = (int(val)- val ) * precision;
+   Serial.println(frac,DEC) ;
+} 
   
 // the loop routine runs over and over again forever:
 void loop() 
@@ -40,9 +57,9 @@ void loop()
   //Serial.print("Raw value:");
   //Serial.println(analogRead(ammeter));
   Serial.print("Vo(V): ");
-  Serial.println(ReadVout(100,3));
+  printDouble(ReadVout(100,3), 1000000);
   Serial.print("Current(A): ");
-  Serial.println(ReadCurrent(100,3));
+  printDouble(ReadCurrent(100,3), 1000000);
   delay(500);        // delay in between reads for stability
 }
 
--- 2015-2016/batteryTester/batteryTester/batteryTester.ino
+++ 2015-2016/batteryTester/batteryTester/batteryTester.ino
@@ -1,6 +1,6 @@
 const int stoppingPin = 9;
 const int relayPin =  8;
-
+const int avgTime = 60;
 
 
 void setup() {
@@ -10,10 +10,19 @@ void setup() {
 }
 
 void loop(){
-  int switchValue = digitalRead(stoppingPin);
-
-  if (buttonState == HIGH) {     
+  
+  int counter = 0;
+  int sumSwitchValue = 0;
+  while(counter < avgTime) {
+    sumSwitchValue += digitalRead(stoppingPin);
+    counter ++;
+    delay(1);
+  }
+  float avgSwitchValue = ((float)(sumSwitchValue))/
+  ((float)(counter));
+  
+  if (avgSwitchValue >= 0.7) {     
     digitalWrite(relayPin, LOW);  
     while(1) {}
   } 
-  
+}
