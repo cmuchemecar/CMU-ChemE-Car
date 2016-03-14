8c618bf242e842483ed7fbc7ed56f7d00122c2ab
Xavier Artache <xaxafour@yahoo.com>
2016-03-14 18:44:31 -0400

    Added voltmeter to battery tester

--- 2015-2016/batteryTester/batteryTester/batteryTester.ino
+++ 2015-2016/batteryTester/batteryTester/batteryTester.ino
@@ -1,12 +1,15 @@
 const int stoppingPin = 9;
 const int relayPin =  8;
 const int avgTime = 60;
+const int voltPin = 10;
 
 
 void setup() {
+  Serial.begin(9600);
   pinMode(relayPin, OUTPUT);      
   pinMode(stoppingPin, INPUT);
   digitalWrite(relayPin, HIGH);  
+  Serial.println("Start");
 }
 
 void loop(){
@@ -20,9 +23,33 @@ void loop(){
   }
   float avgSwitchValue = ((float)(sumSwitchValue))/
   ((float)(counter));
+  Serial.println(VoltMeasurementRaw() + " V");
   
   if (avgSwitchValue >= 0.7) {     
     digitalWrite(relayPin, LOW);  
+    Serial.println("Stop");
     while(1) {}
   } 
+}
+
+String VoltMeasurementRaw(){
+  float volt = analogRead(voltPin);
+  return floatPrint(volt);
+}
+
+String VoltMeasurement(){
+ //return a string representing the voltage
+  float volt = analogRead(voltPin);
+  volt = volt / 1024 * 5; //Arduino scaling
+  volt = volt * 430 / 100; //Voltage divider
+  return floatPrint(volt);
+}
+
+String floatPrint(float x){
+  int intp = x;
+  int fracp = (x - intp) * 1000;
+  if(fracp < 0){
+    fracp = -1 * fracp;
+  }
+  return (String(intp) + "." + String(fracp));
 }
