d42f1af224ba9567d603c02ec46438c45561fb11
Xavier Artache <xaxafour@yahoo.com>
2016-03-22 18:33:42 -0400

    batteryTester now able to send sensor values and save files over bluetooth

--- 2015-2016/BluetoothData/bluetoothSerial.py
+++ 2015-2016/BluetoothData/bluetoothSerial.py
@@ -3,6 +3,7 @@ import time
 import sys
 
 #ser = serial.Serial('/dev/cu.AdafruitEZ-Link4e39-SPP', 9600)
+#ser = serial.Serial('COM27', 9600)
 ser = serial.Serial(str(sys.argv[1]), 9600)
 
 start = False
@@ -17,15 +18,12 @@ while True:
         start = True
     if msg.strip() == "Stop":
         stop = True
-    if (start and not stop):
+    elif (msg.strip() != "Start" and start and not stop):
     	data = msg.split(',')
-    	for i in data:
-    		i.strip()
-    	if data[0] not in files.keys():
-    		dateTime = time.strftime("%d_%m_%Y_%H:%M:%S")
-    		files[data[0]] = open("%s_%s.txt" % (data[0], dateTime), 'w')
-        print (msg)
-        files[data[0]].write('%s, %s' % (data[1], data[2]))
+    	if data[0].strip() not in files.keys():
+    		dateTime = time.strftime("%d_%m_%Y_%H_%M_%S")
+    		files[data[0].strip()] = open("%s_%s.txt" % (data[0].strip(), dateTime), 'w')
+        files[data[0].strip()].write('%s, %s\n' % (data[1].strip(), data[2].strip()))
     if stop:
     	for fileName in files.keys():
         	files[fileName].close()
--- 2015-2016/batteryTester/batteryTester/batteryTester.ino
+++ 2015-2016/batteryTester/batteryTester/batteryTester.ino
@@ -2,17 +2,32 @@ const int stoppingPin = 9;
 const int relayPin =  8;
 const int avgTime = 60;
 const int voltPin = A0;
+const int ammPin = A1;
+const int hallPin = A5;
+
+int hallValue = 0;
+
+unsigned long startTime;
+unsigned long currentTime;
+unsigned long prevPrintTime;
+unsigned long printTime;
 
 
 void setup() {
   Serial.begin(9600);
+  pinMode(hallPin, INPUT);
   pinMode(relayPin, OUTPUT);      
   pinMode(stoppingPin, INPUT);
   digitalWrite(relayPin, HIGH);  
   Serial.println("Start");
+  startTime = millis();
+  prevPrintTime = startTime;
 }
 
 void loop(){
+  currentTime = millis() - startTime;
+  String t = (String) currentTime;
+  printTime = millis() - prevPrintTime;
   
   int counter = 0;
   int sumSwitchValue = 0;
@@ -23,13 +38,36 @@ void loop(){
   }
   float avgSwitchValue = ((float)(sumSwitchValue))/
   ((float)(counter));
-  Serial.println(VoltMeasurement() + " V");
+  
+  if (printTime > 1000) {
+    prevPrintTime = millis();
+    float Vout = ReadVout(100, 3);
+    float current = ReadCurrent(Vout);
+    hallValue = analogRead(hallPin);
+  
+    sendValue("Volt", t, VoltMeasurement());
+    sendValue("Current", t, floatPrint(current*1000));
+    sendValue("Hall", t, (String) hallValue);
+    
+    /*
+    Serial.print(currentTime);
+    Serial.print(" s, ");
+    Serial.print(VoltMeasurement() + " V, ");
+    Serial.print(floatPrint(current*1000) + " mA, ");
+    Serial.print("hallValue = ");
+    Serial.println(hallValue);
+    */
+  }
   
   if (avgSwitchValue >= 0.7) {     
     digitalWrite(relayPin, LOW);  
     Serial.println("Stop");
     while(1) {}
-  } 
+  }
+}
+
+void sendValue(String sensorName, String t, String value) {
+  Serial.println(sensorName + ", " + t + ", " + value);
 }
 
 String VoltMeasurementRaw(){
@@ -53,3 +91,27 @@ String floatPrint(float x){
   }
   return (String(intp) + "." + String(fracp));
 }
+
+float ReadVout(float t, float v)
+  {
+    float total_delay = t;
+    float value_delay = v;
+    float voltage_sum = 0.0, Vout = 0.0;
+    int i = 0, count = 0;
+    while(i<=total_delay)
+    {
+      delay(value_delay);
+      voltage_sum += (analogRead(ammPin)*5./1023.);
+      count++;
+      i += value_delay;
+    }
+    Vout = voltage_sum/count;
+    return Vout;
+  }
+  
+float ReadCurrent(float Vout)
+  {
+    float Vref = 2.85;
+    float current = (Vout - Vref)*(-0.160/125.0);
+    return current*1000;
+  }
