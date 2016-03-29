dc0f83b5a5082e5d3ea248d0d141935a9b2280f2
mnshaw <mnshaw@andrew.cmu.edu>
2016-03-29 16:59:56 -0400

    add folder functionality to bluetoothSerial

--- 2015-2016/BluetoothData/bluetoothSerial.py
+++ 2015-2016/BluetoothData/bluetoothSerial.py
@@ -1,10 +1,16 @@
 import serial
 import time
 import sys
+import os
 
+folder = os.getcwd() + "/" + str(sys.argv[2])
+if not os.path.exists(folder):
+    os.makedirs(folder)
+print folder
 #ser = serial.Serial('/dev/cu.AdafruitEZ-Link4e39-SPP', 9600)
 #ser = serial.Serial('COM27', 9600)
 ser = serial.Serial(str(sys.argv[1]), 9600)
+
 
 start = False
 stop = False
@@ -19,14 +25,17 @@ while True:
     if msg.strip() == "Stop":
         stop = True
     elif (msg.strip() != "Start" and start and not stop):
-    	data = msg.split(',')
-    	if data[0].strip() not in files.keys():
-    		dateTime = time.strftime("%d_%m_%Y_%H_%M_%S")
-    		files[data[0].strip()] = open("%s_%s.txt" % (data[0].strip(), dateTime), 'w')
+        data = msg.split(',')
+        print msg
+        if data[0].strip() not in files.keys():
+            dateTime = time.strftime("%d_%m_%Y_%H_%M_%S")
+            textFile = folder + "/" + ("%s_%s.txt" % (data[0].strip(), dateTime))
+            files[data[0].strip()] = open(textFile, 'w')
         files[data[0].strip()].write('%s, %s\n' % (data[1].strip(), data[2].strip()))
     if stop:
-    	for fileName in files.keys():
-        	files[fileName].close()
+        for fileName in files.keys():
+            textFile = folder + "/" + fileName
+            files[textFile].close()
         break
 
     time.sleep(0.125)
