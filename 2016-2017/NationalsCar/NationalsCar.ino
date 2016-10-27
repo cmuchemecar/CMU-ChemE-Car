2ca2cd955a732edd60abff41fbe0ded94aa87141
Xavier Artache <xaxafour@yahoo.com>
2016-10-27 03:11:27 -0400

    Created functional nationals car program

--- 2016-2017/NationalsCar/NationalsCar.ino
+++ 2016-2017/NationalsCar/NationalsCar.ino
@@ -1,15 +1,94 @@
-#define switchpin 7
+#include <Data.h>
+#include <SD.h>
+#include <SPI.h>
+
+#define WHEELHALLPIN 5
+#define STOPHALLPIN 6
+#define KILLSWITCHPIN 12
+#define RELAYPIN 10
+
+int start = 0;
+
+int magnets = 1;
+float radius = 0.068;
+float circumference = 3.14 * 2 * radius;
+float x = 0;
+float dx = circumference / magnets;
+float dt = 0;
+float prevTime = 0;
+float avgSpeed = 0;
+long readings = 0;
+
+int prevWheelHall = 1;
+int currWheelHall = 1;
+
+Sensor wheelHall = Data.sensor("Wheel", WHEELHALLPIN);
+Sensor stopHall = Data.sensor("Stop", STOPHALLPIN);
+Sensor killswitch = Data.sensor("Kill", KILLSWITCHPIN);
 
 void setup() {
-  pinMode(switchpin, OUTPUT);
-  Serial.begin(9600);
-  digitalWrite(switchpin, LOW);
-  delay(1000);
+  pinMode(RELAYPIN, OUTPUT);
+  digitalWrite(RELAYPIN, LOW);
+  
+  while(!start) {
+    if (!killswitch.readDigital()) {
+      start = 1;
+    }
+    delay(100); 
+  }
+  
+  Data.beginSerial();
+  Data.beginTimer();
+  Data.debugBluetooth("Collecting data...");
+  Data.startBluetooth();
+  
+  prevTime = Data.currentTime();
 }
 
 void loop() {
-  digitalWrite(switchpin, HIGH);
-  delay(1000);
-  digitalWrite(switchpin, LOW);
-  delay(1000);
+  
+  if (!killswitch.readDigital()) {
+    digitalWrite(RELAYPIN, LOW);
+    Data.stopBluetooth();
+    Data.freeze();
+  }
+  
+  if (stopHall.readDigital()) {
+    digitalWrite(RELAYPIN, HIGH);
+  }
+  else {
+    digitalWrite(RELAYPIN, LOW);
+    Data.debugBluetooth("--------------");
+    Data.debugBluetooth("Trial time");
+    Data.debugBluetooth(Data.floatToString(Data.currentTime()));
+    Data.debugBluetooth("Estimated total distance");
+    Data.debugBluetooth(Data.floatToString(x));
+    Data.debugBluetooth("Esitmated average speed");
+    Data.debugBluetooth(Data.floatToString((avgSpeed / readings)));
+    Data.stopBluetooth();
+    Data.freeze();
+  }
+  
+  Data.println(wheelHall.readDigital());
+  prevWheelHall = currWheelHall;
+  currWheelHall = wheelHall.readDigital();
+  if ((!prevWheelHall) && currWheelHall) {
+    dt = Data.currentTime() - prevTime;
+    prevTime = Data.currentTime();
+    x += dx;
+    avgSpeed += dx/dt;
+    readings++;
+      
+    Data.debugBluetooth(Data.dataLine(
+      "Distance",
+      Data.floatToString(Data.currentTime()),
+      Data.floatToString(x)));
+      
+    Data.debugBluetooth(Data.dataLine(
+      "Speed",
+      Data.floatToString(Data.currentTime()),
+      Data.floatToString(dx/dt)));
+  }
+
+  delay(100);
 }
